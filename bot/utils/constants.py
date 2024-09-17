f4804ff1702f4911cf56c11cd1a7ba31b25e811b
suntrackspb <spbwar@gmail.com>
2024-09-18 00:40:24 +0300

    Add user status message function and update bot button options.

--- bot/utils/constants.py
+++ bot/utils/constants.py
@@ -1,10 +1,29 @@
 from enum import Enum
 
+from api.schemas.user import UserSchema
+
 
 class BotButtons(Enum):
     NEXT = 'Next Boss'
     TODAY = 'Today Bosses'
     WEEK = 'Weekly Bosses'
     NOTIFY = 'Notification'
-    SETTINGS = 'Settings'
+    PROMO = 'Promo codes'
+    ADD_PROMO = 'Add promo code'
     HELP = 'Help'
+
+
+def get_status_message(user: UserSchema):
+    return f"""
+<b>{"🟢" if user.is_subscribed else "🔴"}</b> Bosses
+
+<b>{"🟢" if user.is_promoted else "🔴"}</b> Promo codes
+
+<b>{"🟢" if user.push1 else "🔴"}</b> Push 1 min
+
+<b>{"🟢" if user.push5 else "🔴"}</b> Push 5 min
+
+<b>{"🟢" if user.push10 else "🔴"}</b> Push 10 min
+
+<b>{"🟢" if user.push30 else "🔴"}</b> Push 30 min
+    """
