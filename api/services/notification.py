248030c0fd8568e1618ec5190496e8d7bdb1cc66
suntrackspb <spbwar@gmail.com>
2024-09-18 00:41:07 +0300

    Refactor time calculation logic and remove commented-out code.

--- api/services/notification.py
+++ api/services/notification.py
@@ -26,38 +26,22 @@ class NotificationService:
         current_time = datetime.now().strftime("%H:%M")
         boss_time = datetime.strptime(boss.boss_time, "%H:%M")
 
-        current_time = datetime.strptime("11:50", "%H:%M")
+        # current_time = datetime.strptime("11:50", "%H:%M")
 
         if boss.boss_time == "00:00":
             boss_time = boss_time + timedelta(days=1)
 
         # Преобразуем текущие время и время босса в объекты datetime
-        # current_time_dt = datetime.strptime(current_time, "%H:%M")
+        current_time_dt = datetime.strptime(current_time, "%H:%M")
 
-        # print(boss_time, current_time)
+        time_difference = (boss_time - current_time_dt).total_seconds() / 60  # разница в минутах
 
-        # Рассчитываем разницу во времени
-        time_difference = (boss_time - current_time).total_seconds() / 60  # разница в минутах
-
-        print(time_difference)
+        # print(time_difference, datetime.now().strftime("%H:%M:%S"))
 
         if time_difference in [1, 5, 10, 30]:
             print(f'Difference: {time_difference} minutes')
             data = await self.crud.notify_list(boss_id=boss.id, user_push=Push[time_difference])
             print("===", data)
             return data
-
-        # if time_difference == 30:
-        #     print("Разница 30 минут")
-        #     return await self.crud.notify_list(boss_id=boss.id, push=User.push30)
-        # elif time_difference == 10:
-        #     print("Разница 10 минут")
-        #     return await self.crud.notify_list(boss_id=boss.id, push=User.push10)
-        # elif time_difference == 5:
-        #     print("Разница 5 минут")
-        #     return await self.crud.notify_list(boss_id=boss.id, push=User.push5)
-        # elif time_difference == 1:
-        #     print("Разница 1 минут")
-        #     return await self.crud.notify_list(boss_id=boss.id, push=User.push1)
 
         return []
