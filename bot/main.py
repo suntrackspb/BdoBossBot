0a73fac098ab6a99f8684f43d81ec63a21fa2a12
suntrackspb <spbwar@gmail.com>
2024-09-20 19:06:36 +0300

    Refactor bot initialization to support session management and new routes and add proxy.

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import BotCommand
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
@@ -8,8 +9,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from environs import Env
 
-from api.schemas.notification import NotificationSchema, BossNotificationSchema
-from bot.handlers import default_router, callback_router
+from api.config import config
+from api.schemas.notification import BossNotificationSchema
+from bot.handlers import callback_router, default_router, button_router, promo_router
 from bot.middleware.web_api_middleware import WebApiMiddleware
 from bot.utils.http_client import HttpClient
 
@@ -20,7 +22,9 @@ bot_token = env('BOT_TOKEN')
 web_api_url = env('WEB_API_URL')
 http_client = HttpClient(api_url=web_api_url, api_key=bot_token)
 
-bot = Bot(bot_token, default=DefaultBotProperties(parse_mode='HTML'))
+session = AiohttpSession(proxy=config.proxy.URL) if config.proxy.URL else None
+
+bot = Bot(bot_token, session=session, default=DefaultBotProperties(parse_mode='HTML'))
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
@@ -29,7 +33,9 @@ dp.callback_query.middleware(CallbackAnswerMiddleware())
 
 dp.include_routers(
     callback_router,
-    default_router
+    button_router,
+    promo_router,
+    default_router,
 )
 
 
@@ -37,6 +43,7 @@ async def set_bot_commands() -> None:
     await bot.set_my_commands(
         [
             BotCommand(command="/start", description="Register the bot"),
+            BotCommand(command="/reload", description="Reload keyboards"),
             BotCommand(command="/settings", description="Open settings WebApp"),
             BotCommand(command="/id", description="Get the user and chat ids"),
         ],
@@ -58,8 +65,6 @@ async def send_notification(bot_: Bot, http_client_: HttpClient) -> None:
     except AttributeError:
         print("Empty data")
 
-    # await bot.send_message(chat_id=CHAT_ID, text="Это ваше запланированное уведомление!")
-
 
 @dp.startup()
 async def on_startup() -> None:
@@ -70,9 +75,8 @@ async def on_startup() -> None:
 async def main() -> None:
     try:
         scheduler = AsyncIOScheduler()
-        scheduler.add_job(send_notification, "interval", seconds=5, args=(bot, http_client))
-        # trigger = CronTrigger(second='1')
-        # scheduler.add_job(send_notification, args=(bot, http_client))
+        trigger = CronTrigger(second='1')
+        scheduler.add_job(send_notification, trigger=trigger, args=(bot, http_client))
         scheduler.start()
         await dp.start_polling(bot, polling_timeout=3)
     finally:
